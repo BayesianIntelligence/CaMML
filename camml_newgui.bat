e913853b10503ba4ea4b221dd60c277c6f581e41
Steven Mascaro <sm@voracity.org>
2013-03-18 17:22:26 +1100

    Changes to make downloading and starting CaMML simple.

--- camml_newgui.bat
+++
@@ -1,4 +0,0 @@
-REM Batch file to run CaMML under windows
-path=%path%;lib
-echo %path%
-java -Xmx512m -Djava.library.path=lib -classpath Camml;jar\cdms.jar;jar\NeticaJ.jar;jar\junit.jar;jar\weka.jar;.\;jar\tetrad4.jar camml.core.newgui.RunGUI
